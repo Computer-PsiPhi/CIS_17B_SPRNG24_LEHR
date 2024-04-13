dc8fa8ec00828da70b766967b7a2bf263d481dab
Computer PsiPhi <blotted.209@gmail.com>
2024-04-13 04:08:55 -0700

    latest push

--- Projects/CIS_17B_PERSONAL_PROJECT/17B_Personal_Project_Estore_V2/build/Debug/Cygwin-Windows/Store.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/Cygwin-Windows/Store.o: Store.cpp Store.h Item.h
-Store.h:
-Item.h:
--- Projects/CIS_17B_PERSONAL_PROJECT/17B_Personal_Project_Estore_V2/build/Debug/Cygwin-Windows/User.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/User.o: User.cpp User.h Item.h Cart.h Admin.h
-User.h:
-Item.h:
-Cart.h:
-Admin.h:
